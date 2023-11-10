dcf1b585ba77ded966f1673acf51d8a155f5a6d4
ilyas <muhammadilyas827827@gmail.com>
2023-11-10 06:30:52 -0800

    some error fixed

--- lib/views/favorite/favorite_screen.dart
+++ lib/views/favorite/favorite_screen.dart
@@ -5,13 +5,24 @@ import 'package:travelmatex/utils/colors/colors.dart';
 import 'package:travelmatex/utils/constants/constants.dart';
 import 'package:travelmatex/views/widgets/favorite_icon_widget/favorite_icon_widget.dart';
 
-class FavoriteScreen extends StatelessWidget {
-  FavoriteScreen({super.key});
-  final favoriteController = Get.find<FavoriteController>();
+class FavoriteScreen extends StatefulWidget {
+  const FavoriteScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  late FavoriteController favoriteController;
+  @override
+  void initState() {
+    favoriteController = Get.find<FavoriteController>();
+    favoriteController.initFavorites();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    favoriteController.initFavorites();
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -25,116 +36,133 @@ class FavoriteScreen extends StatelessWidget {
         ),
         const Divider(),
         FutureBuilder(
-            future: Future.delayed(const Duration(seconds: 1)),
-            builder: (context, data) {
-              if (data.connectionState == ConnectionState.waiting) {
-                return const Expanded(
-                  child: Center(
-                    child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation(
-                            Color.fromARGB(255, 3, 40, 70))),
-                  ),
-                );
-              }
-              return Obx(() {
-                if (favoriteController.favorites.isEmpty) {
-                  return Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(width: double.infinity),
-                        Text(
-                          'No favorites added yet?',
-                          style: googleFontStyle(
-                              fontsize: 23, fontweight: FontWeight.w700),
-                        ),
-                        Text(
-                          'Your favorites list is waiting to be filled!',
-                          style: googleFontStyle(
-                              fontsize: 17, fontweight: FontWeight.w300),
-                        )
-                      ],
+          future: Future.delayed(const Duration(seconds: 1)),
+          builder: (context, data) {
+            if (data.connectionState == ConnectionState.waiting) {
+              return const Expanded(
+                child: Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(
+                      Color.fromARGB(255, 3, 40, 70),
                     ),
-                  );
-                }
-                return Expanded(
-                  child: ListView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    itemCount: favoriteController.favorites.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Container(
-                          height: screenHeight * 0.25,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  image: NetworkImage(favoriteController
-                                      .favorites[index]
-                                      .destinationImageUrls[0]),
-                                  fit: BoxFit.cover),
-                              borderRadius: BorderRadius.circular(25)),
-                          child: Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                height: screenHeight * 0.064,
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                    color: whiteColorWithOpacity50,
-                                    borderRadius: BorderRadius.circular(20)),
-                                child: Padding(
-                                  padding: EdgeInsets.symmetric(
-                                      horizontal: screenHeight * 0.01,
-                                      vertical: screenHeight * 0.005),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            favoriteController.favorites[index]
-                                                .destinationName,
-                                            style: googleFontStyle(
-                                              fontweight: FontWeight.bold,
-                                              fontsize: MediaQuery.of(context)
-                                                      .textScaleFactor *
-                                                  17,
-                                              color: const Color.fromARGB(
-                                                  176, 0, 0, 0),
-                                            ),
-                                          ),
-                                          Text(
-                                              "${favoriteController.favorites[index].destinationDistrict}, ${favoriteController.favorites[index].destinationCategory}",
-                                              style: googleFontStyle(
-                                                  fontsize:
-                                                      MediaQuery.of(context)
-                                                              .textScaleFactor *
-                                                          14,
-                                                  fontweight: FontWeight.w500))
-                                        ],
-                                      ),
-                                      FavoriteIcon(
-                                          destinationId: favoriteController
-                                              .favorites[index].id!)
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
+                  ),
+                ),
+              );
+            }
+            return buildFavoritesList(context);
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget buildFavoritesList(BuildContext context) {
+    return Obx(() {
+      if (favoriteController.favorites.isEmpty) {
+        return buildEmptyFavorites();
+      }
+      return buildNonEmptyFavorites(context);
+    });
+  }
+
+  Widget buildEmptyFavorites() {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(width: double.infinity),
+          Text(
+            'No favorites added yet?',
+            style: googleFontStyle(fontsize: 23, fontweight: FontWeight.w700),
+          ),
+          Text(
+            'Your favorites list is waiting to be filled!',
+            style: googleFontStyle(fontsize: 17, fontweight: FontWeight.w300),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildNonEmptyFavorites(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        itemCount: favoriteController.favorites.length,
+        itemBuilder: (context, index) {
+          return buildFavoriteItem(context, index);
+        },
+      ),
+    );
+  }
+
+  Widget buildFavoriteItem(BuildContext context, int index) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        height: screenHeight * 0.25,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+              favoriteController.favorites[index].destinationImageUrls[0],
+            ),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: EdgeInsets.all(screenHeight * 0.01),
+            child: Container(
+              height: screenHeight * 0.064,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: whiteColorWithOpacity50,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: screenHeight * 0.01,
+                  vertical: screenHeight * 0.005,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          favoriteController.favorites[index].destinationName,
+                          style: googleFontStyle(
+                            fontweight: FontWeight.bold,
+                            fontsize:
+                                MediaQuery.of(context).textScaleFactor * 17,
+                            color: const Color.fromARGB(176, 0, 0, 0),
                           ),
                         ),
-                      );
-                    },
-                  ),
-                );
-              });
-            })
-      ],
+                        Text(
+                          "${favoriteController.favorites[index].destinationDistrict}, ${favoriteController.favorites[index].destinationCategory}",
+                          style: googleFontStyle(
+                            fontsize:
+                                MediaQuery.of(context).textScaleFactor * 14,
+                            fontweight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    FavoriteIcon(
+                      destinationId: favoriteController.favorites[index].id!,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
